f03fa128c01c4f02d571d1d582da2ed82a466cbf
joaotemponi <150601366+joaotemponi@users.noreply.github.com>
2024-03-14 11:40:50 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,15 @@
+/*
+1- Complete os espaços com x com as váriaveis nome, idade...
+Mesmo que não tenha opções de lazer e nem graduação faça atividade
+utilizando algum lazer e curso, seja respeitoso com as opções.
+nome: João Pedro Temponi de Jesus
+
+*/
+var meuNome = "Jo\u00E3o Temponi";
+var minhaIdade = 16;
+var lazerUm = "jogar videogame";
+var lazerDois = "aprender progama\u00E7\u00E3o";
+var lazerFavorito = "aprender hacking";
+var objetivoUm = "engenharia de software";
+var objetivoDois = "piloto da FAB";
+console.log("Bom Dia!! Meu nome \u00E9 ".concat(meuNome, ", a minha idade \u00E9 ").concat(minhaIdade, ",\n tenho como momento de lazer ").concat(lazerUm, ", ").concat(lazerDois, " e o que mais gosto \u00E9 ").concat(lazerFavorito, ".\nPretendo ingressar em uma universidade, at\u00E9 esse momento\nestou tendencioso(a) a fazer ").concat(objetivoUm, ", minha segunda op\u00E7\u00E3o \u00E9 ").concat(objetivoDois, "."));
---
+++ atividade.ts
@@ -0,0 +1,22 @@
+/*
+1- Complete os espaços com x com as váriaveis nome, idade...
+Mesmo que não tenha opções de lazer e nem graduação faça atividade
+utilizando algum lazer e curso, seja respeitoso com as opções.
+nome: João Pedro Temponi de Jesus
+
+*/
+
+
+let meuNome : string = `João Temponi`;
+let minhaIdade : number = 16;
+let lazerUm : string = `jogar videogame`;
+let lazerDois : string = `aprender progamação`;
+let lazerFavorito : string = `aprender hacking`;
+let objetivoUm : string = `engenharia de software`;
+let objetivoDois : string = `piloto da FAB`;
+
+console.log(`Bom Dia!! Meu nome é ${meuNome}, a minha idade é ${minhaIdade},
+tenho como momento de lazer ${lazerUm}, ${lazerDois}
+e o que mais gosto é ${lazerFavorito}.
+Pretendo ingressar em uma universidade, até esse momento
+estou tendencioso(a) a fazer ${objetivoUm}, minha segunda opção é ${objetivoDois}.`)
